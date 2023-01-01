2d01eb7067c88139567487d7d360fa1c3754d42a
Marius S. Riseth <marius.s.riseth@pm.me>
2023-01-01 17:32:26 +0100

    Animasjonsknapp

--- script/dag-natt.js
+++ script/dag-natt.js
@@ -1,30 +1,53 @@
 let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#dark-mode-toggle");
 
-// sjekk om natt-modus er på
-// hvis natt-modus er på, skru det av
-// hvis natt-modus er av, skru det på
 
+const DtN = () => {
+  document.querySelector(".moon").classList.remove("moonAnimRev");
+  document.querySelector(".moon").classList.add("moonAnim");
+  const suns = document.getElementsByClassName("sun");
+    for (let i = 0; i < suns.length; i++) {
+      let element = suns[i];
+      element.classList.remove("sunAnimRev");
+      element.classList.add("sunAnim");
+    }
+  }
+
+const NtD = () => {
+  document.querySelector(".moon").classList.remove("moonAnim");
+  document.querySelector(".moon").classList.add("moonAnimRev");
+  const suns = document.getElementsByClassName("sun");
+    for (let i = 0; i < suns.length; i++) {
+      let element = suns[i];
+      element.classList.remove("sunAnim");
+      element.classList.add("sunAnimRev");
+    }
+  }
+
+// hvis natt-modus er på, skru det av
 const enableDarkMode = () => {
   // 1. legg til en class "darkmode" til body
   document.body.classList.add("darkmode");
+  DtN();
   // 2. oppdater dark mode i det lokale minnet
   localStorage.setItem("darkMode", "enabled");
 };
-
+// hvis natt-modus er av, skru det på
 const disableDarkMode = () => {
-  // 1. legg til en class "darkmode" til body
+  // 1. fjern class "darkmode" til body
   document.body.classList.remove("darkmode");
-  // 2. oppdater dark mode i det lokale minnet
+  NtD();
+  // 2. oppdater fravær av dark mode i det lokale minnet
   localStorage.setItem("darkMode", null);
 };
-
+// sjekk om natt-modus er på
 if (darkMode === "enabled") {
-  enableDarkMode();
-}
+   enableDarkMode();
+ }
 
 darkModeToggle.addEventListener("click", () => {
   darkMode = localStorage.getItem("darkMode");
+
   if (darkMode !== "enabled") {
     enableDarkMode();
     console.log(darkMode);
