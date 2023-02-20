9206f8232665e43110b563838381a63e75d05c96
Marius S. Riseth <marius.s.riseth@pm.me>
2023-02-20 19:41:26 +0100

    Fungerende animasjon

---
+++ ressursbank/ny-rb-seksjoner.js
@@ -0,0 +1,43 @@
+// Henter alle knappelementene
+const ifKnapper = document.querySelectorAll('.ifKnapp');
+
+// Lagrer en referanse til den forrige synlige seksjonen
+let forrigeSeksjon = null;
+
+// Legger til en hendelseslytter for klikk på hver knapp
+ifKnapper.forEach(knapp => {
+  knapp.addEventListener('click', () => {
+    // Henter ID-en til innholdsseksjonen som tilsvarer knappen
+    const seksjonID = knapp.dataset.seksjon;
+
+    // Henter ID-en til knappen som ble klikket på
+    const knappID = knapp.dataset.knapp;
+
+    // Henter den tilsvarende seksjonen
+    const seksjon = document.querySelector('.ifSeksjon[data-seksjon="' + knappID + '"]');
+
+    // Skjuler den forrige synlige seksjonen hvis det finnes en, med mindre det er den samme seksjonen som den vi klikket på
+    if (forrigeSeksjon !== null && forrigeSeksjon !== seksjon) {
+      forrigeSeksjon.style.maxHeight = '0px';
+      forrigeSeksjon.classList.remove('synlig');
+    }
+
+    // Hvis seksjonen allerede er synlig, skjul den
+    if (seksjon.classList.contains('synlig')) {
+      seksjon.style.maxHeight = '0px';
+      seksjon.classList.remove('synlig');
+    } else { // Ellers gjør den synlig
+      // Mål høyden på seksjonen og sett max-height til denne verdien
+      const høyde = seksjon.scrollHeight;
+      seksjon.style.maxHeight = høyde + 'px';
+
+      // Gjør seksjonen synlig
+      seksjon.classList.add('synlig');
+    }
+
+    // Oppdaterer referansen til forrige synlige seksjon
+    forrigeSeksjon = seksjon;
+
+    console.log(`Klikk på ${knappID}. Seksjon synlighet: ${seksjon.classList.contains('synlig')}`);
+  });
+});
