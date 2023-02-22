12feb47ac06f951d12ed267e3088862af83fdff4
Marius S. Riseth <marius.s.riseth@pm.me>
2023-02-22 03:00:47 +0100

    Fungerende bildekarusell

--- ressursbank/rb-bildekarusell.js
+++ ressursbank/rb-bildekarusell.js
@@ -1,20 +1,30 @@
-// Henter bildekarusell-elementet fra HTML
-const bildekarusell = document.querySelector('.bildekarusell');
+// Henter bildefremvisning-elementet fra HTML
+const bildefremvisning = document.querySelector('.bildefremvisning');
 
 // Henter alle miniatyrbilder fra HTML
 const miniatyrbilder = document.querySelectorAll('.miniatyrbilde');
 
+// Henter bakgrunnsområdet for bildekarusellen fra HTML-dokumentet
+const kryss = document.querySelector('.kryss');
+
+
 // Går gjennom alle miniatyrbilder og legger til klikkhendelseslyttere
 miniatyrbilder.forEach((miniatyrbilde) => {
-    // Når et miniatyrbilde klikkes på, viser bildekarusellen og setter riktig bilde
+    // Når et miniatyrbilde klikkes på, viser bildefremvisningen og setter riktig bilde
     miniatyrbilde.addEventListener('click', () => {
-        bildekarusell.style.display = 'flex'; // viser bildekarusellen når et miniatyrbilde klikkes på
-        visBilde(miniatyrbilde.src); // viser riktig bilde i bildekarusellen
+        bildefremvisning.style.display = 'flex'; // viser bildefremvisningen når et miniatyrbilde klikkes på
+        visBilde(miniatyrbilde.src); // viser riktig bilde i bildefremvisningen
     });
 });
 
-// Hjelpefunksjon for å vise riktig bilde i bildekarusellen
+// Legger til klikk-lytter på kryss-ikonet
+kryss.addEventListener('click', () => {
+  // Skjuler bildekarusellen hvis klikket ble gjennomført
+  bildefremvisning.style.display = 'none';
+});
+
+// Hjelpefunksjon for å vise riktig bilde i bildefremvisningen
 function visBilde(bildeSrc) {
-  const visningsBilde = bildekarusell.querySelector('img');
-  visningsBilde.src = bildeSrc;
+  const visningsBilde = bildefremvisning.querySelector('.fokusvindu');
+  visningsBilde.innerHTML = `<img src="${bildeSrc}" alt="bilde">`;
 }
