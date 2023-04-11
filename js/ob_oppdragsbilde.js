d1041bb7bbeb0925243aa0c5b57cc3ae012ed4ef
Marius S. Riseth <marius.s.riseth@pm.me>
2023-04-11 20:02:38 +0200

    HovedmenyFiks

--- js/ob_oppdragsbilde.js
+++ js/ob_oppdragsbilde.js
@@ -4,16 +4,20 @@ function sizeSpacer(spacerElement) {
    var img = spacerElement.nextElementSibling || spacerElement.nextSibling;
    var lastContentNode = container.children[container.children.length - 1];
 
-   var h = container.clientHeight - img.offsetHeight - -10; // endring: legger til en margin på 10px mellom img og spacer
+   var h = container.clientHeight - img.offsetHeight;
    spacerElement.style.height = h + "px";
 
    if (h > 0) {
       while (img.offsetHeight > lastContentNode.offsetHeight && h > 0) {
-         // endring: sammenligner høyden på img og lastContentNode
          h--;
          spacerElement.style.height = h + "px";
       }
    }
+
+   // Legg til disse linjene
+   var oppdragsInfo = container.querySelector(".oppdragsInfo");
+   var availableHeight = oppdragsInfo.clientHeight - img.offsetHeight;
+   spacerElement.style.height = Math.max(0, availableHeight) + "px";
 }
 
 onload = onresize = function () {
