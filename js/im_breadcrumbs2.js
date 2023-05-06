2b2071e0e364560b11d372698a281888d83c5b16
Marius S. Riseth <marius.s.riseth@pm.me>
2023-05-06 13:23:48 +0200

    Starten på hovedmenyen

---
+++ js/im_breadcrumbs2.js
@@ -0,0 +1,243 @@
+const siteHierarchy = {
+   "/index.html": {
+      displayName: "Hjem",
+      children: {
+         "/timeplan.html": {
+            displayName: "Timeplan",
+            children: {
+               "/timeplan/uke_1.html": { displayName: "Uke 1" },
+               "/timeplan/uke_2.html": { displayName: "Uke 2" },
+               "/timeplan/uke_3.html": { displayName: "Uke 3" },
+               "/timeplan/uke_4.html": { displayName: "Uke 4" },
+               "/timeplan/uke_5.html": { displayName: "Uke 5" },
+               "/timeplan/uke_6.html": { displayName: "Uke 6" },
+               "/timeplan/uke_7.html": { displayName: "Uke 7" },
+               // Vinterferie
+               "/timeplan/uke_9.html": { displayName: "Uke 9" },
+               "/timeplan/uke_10.html": { displayName: "Uke 10" },
+               "/timeplan/uke_11.html": { displayName: "Uke 11" },
+               "/timeplan/uke_12.html": { displayName: "Uke 12" },
+               "/timeplan/uke_13.html": { displayName: "Uke 13" },
+               // Påskseferie
+               "/timeplan/uke_15.html": { displayName: "Uke 15" },
+               "/timeplan/uke_16.html": { displayName: "Uke 16" },
+               "/timeplan/uke_17.html": { displayName: "Uke 17" },
+               "/timeplan/uke_18.html": { displayName: "Uke 18" },
+               "/timeplan/uke_19.html": { displayName: "Uke 19" },
+               "/timeplan/uke_20.html": { displayName: "Uke 20" },
+               "/timeplan/uke_21.html": { displayName: "Uke 21" },
+               "/timeplan/uke_22.html": { displayName: "Uke 22" },
+               "/timeplan/uke_23.html": { displayName: "Uke 23" },
+               "/timeplan/uke_24.html": { displayName: "Uke 24" },
+               "/timeplan/uke_25.html": { displayName: "Uke 25" },
+               // Sommerferie
+               "/timeplan/uke_34.html": { displayName: "Uke 34" },
+               "/timeplan/uke_35.html": { displayName: "Uke 35" },
+               "/timeplan/uke_36.html": { displayName: "Uke 36" },
+               "/timeplan/uke_37.html": { displayName: "Uke 37" },
+               "/timeplan/uke_38.html": { displayName: "Uke 38" },
+               "/timeplan/uke_39.html": { displayName: "Uke 39" },
+               "/timeplan/uke_40.html": { displayName: "Uke 40" },
+               "/timeplan/uke_41.html": { displayName: "Uke 41" },
+               "/timeplan/uke_42.html": { displayName: "Uke 42" },
+               "/timeplan/uke_43.html": { displayName: "Uke 43" },
+               "/timeplan/uke_44.html": { displayName: "Uke 44" },
+               "/timeplan/uke_45.html": { displayName: "Uke 45" },
+               "/timeplan/uke_46.html": { displayName: "Uke 46" },
+               "/timeplan/uke_47.html": { displayName: "Uke 47" },
+               "/timeplan/uke_48.html": { displayName: "Uke 48" },
+               "/timeplan/uke_49.html": { displayName: "Uke 49" },
+               "/timeplan/uke_50.html": { displayName: "Uke 50" },
+               "/timeplan/uke_51.html": { displayName: "Uke 51" },
+               // Juleferie
+            },
+         },
+         "/oppdragsbank.html": {
+            displayName: "Oppdragsbank",
+            children: {
+               "/oppdragsbank/1_grafisk-design.html": {
+                  displayName: "Grafisk design",
+                  children: {
+                     "/oppdragsbank/1_grafisk-design/1-1-1_illustrator-pen-tool.html": { displayName: "Illustrator: Pen tool" },
+                     "/oppdragsbank/1_grafisk-design/1-2-1_produkt-etter-manualen.html": { displayName: "Produkt etter manualen" },
+                     "/oppdragsbank/1_grafisk-design/1-2-2_serie-etter-manualen.html": { displayName: "Serie etter manualen" },
+                     "/oppdragsbank/1_grafisk-design/1-2-3_fiktiv-virksomhet.html": { displayName: "Fiktiv virksomhet" },
+                     "/oppdragsbank/1_grafisk-design/1-1-2_deg-selv-i-vektor.html": { displayName: "Deg selv i vektor" },
+                     "/oppdragsbank/1_grafisk-design/1-2-4_im-i-vektor.html": { displayName: "IM-lærerne i vektor" },
+                  },
+               },
+               "/oppdragsbank/2_3d-design.html": {
+                  displayName: "3D Grafikk",
+                  children: {
+                     "/oppdragsbank/2_3d-design/2-2-1_modeller-spillobjekt.html": { displayName: "Modeller et spillobjekt" },
+                  },
+               },
+               "/oppdragsbank/3_fotografi.html": {
+                  displayName: "Fotografi",
+                  children: {
+                     "/oppdragsbank/3_fotografi/3-1-1_naturens-barn.html": { displayName: "Naturens barn" },
+                     "/oppdragsbank/3_fotografi/3-2-1_filmplakat.html": { displayName: "Filmplakat" },
+                     "/oppdragsbank/3_fotografi/3-2-2_gatefoto.html": { displayName: "Gatefoto" },
+                  },
+               },
+               "/oppdragsbank/4_film-og-vfx.html": {
+                  displayName: "Film og VFX",
+                  children: {
+                     "/oppdragsbank/4_film-og-vfx/4-2-1_supersmooth-reklamefilm.html": { displayName: "Reklamefilm" },
+                     "/oppdragsbank/4_film-og-vfx/4-2-2_manus123.html": { displayName: "Manus 1-2-3" },
+                  },
+               },
+               "/oppdragsbank/5_animasjon.html": {
+                  displayName: "Animasjon",
+                  children: {
+                     "/oppdragsbank/5_animasjon/5-2-1_tine-animasjon.html": { displayName: "Litagoreklame" },
+                  },
+               },
+               "/oppdragsbank/6_lyddesign.html": {
+                  displayName: "Lyddesign",
+                  children: {
+                     "/oppdragsbank/6_lyddesign/6-2-1_im-podcast.html": { displayName: "IM-Podkast" },
+                     "/oppdragsbank/6_lyddesign/6-2-2_musicless-music-video.html": { displayName: "Musicless music video" },
+                  },
+               },
+               "/oppdragsbank/7_drift.html": {
+                  displayName: "Drift",
+                  children: {
+                     "/oppdragsbank/7_drift/7-2-1_burp-suite.html": { displayName: "Burp Suite" },
+                     "/oppdragsbank/7_drift/7-2-2_bash-script.html": { displayName: "Bash Script" },
+                     // ...
+                  },
+               },
+               "/oppdragsbank/8_programmering.html": {
+                  displayName: "Programmering",
+                  children: {
+                     "/oppdragsbank/8_programmering/8-2-1_pihole-blokade.html": { displayName: "PiHole Blokade" },
+                  },
+               },
+               "/oppdragsbank/9_webutvikling.html": {
+                  displayName: "Webutvikling",
+                  children: {
+                     "/oppdragsbank/9_webutvikling/9-2-1_reklamefilmnettside.html": { displayName: "Reklamefilmnettside" },
+                     "/oppdragsbank/9_webutvikling/9-2-2_kantinenettside.html": { displayName: "Kantinenettside" },
+                     "/oppdragsbank/9_webutvikling/9-2-3_kantinemeny.html": { displayName: "Kantinemeny" },
+                     "/oppdragsbank/9_webutvikling/9-2-4_abstrakt-kunst.html": { displayName: "Abstrakt kunst" },
+                     "/oppdragsbank/9_webutvikling/9-2-6_datamattebase.html": { displayName: "Datamattebase" },
+                  },
+               },
+               "/oppdragsbank/10_ai-systemer.html": {
+                  displayName: "AI-systemer",
+                  //   children: { "oppdragnavn.html": { displayName: "oppdragnavn" } },
+               },
+            },
+         },
+         "/ressursbank.html": {
+            displayName: "IM-fagverket",
+         },
+      },
+   },
+};
+
+function generateBreadcrumbs() {
+   const currentPath = window.location.pathname; // Henter gjeldende nettadresse
+   const breadcrumbsList = document.getElementById("breadcrumbs"); // Henter breadcrumbs-listeelementet
+
+   // Funksjon for å finne stien til gjeldende side i siteHierarchy
+   function findPathInHierarchy(hierarchy, currentPath, path = []) {
+      for (const [key, value] of Object.entries(hierarchy)) {
+         const newPath = [...path, { displayName: value.displayName, url: key }];
+         if (key === currentPath) {
+            return newPath;
+         }
+         if (value.children) {
+            const result = findPathInHierarchy(value.children, currentPath, newPath);
+            if (result) return result;
+         }
+      }
+      return null;
+   }
+
+   // Kaller funksjonen for å finne stien til gjeldende side
+   const path = findPathInHierarchy(siteHierarchy, currentPath);
+
+   if (!path) return; // Avbryter hvis stien ikke ble funnet
+
+   // Genererer liste-elementer for brødsmulene
+   path.forEach((item, index) => {
+      const listItem = document.createElement("li");
+      if (index < path.length - 1) {
+         // Hvis det ikke er det siste elementet, opprett en lenke
+         const link = document.createElement("a");
+         link.href = item.url;
+         link.textContent = item.displayName;
+         listItem.appendChild(link);
+      } else {
+         // Hvis det er det siste elementet, viser det som tekst
+         listItem.textContent = item.displayName;
+      }
+      breadcrumbsList.appendChild(listItem);
+   });
+
+   // Funksjon for å oppdatere visningen av liste-elementene basert på vindusbredden
+   function updateBreadcrumbsVisibility() {
+      const windowWidth = window.innerWidth;
+
+      if (windowWidth <= 600) {
+         const listItems = breadcrumbsList.getElementsByTagName("li");
+         for (let i = 0; i < listItems.length - 3; i++) {
+            listItems[i].style.display = "none";
+         }
+      } else {
+         const listItems = breadcrumbsList.getElementsByTagName("li");
+         for (let i = 0; i < listItems.length; i++) {
+            listItems[i].style.display = "list-item";
+         }
+      }
+   }
+   // Kaller funksjonen for å oppdatere visningen av liste-elementene
+   updateBreadcrumbsVisibility();
+
+   // Legger til en event listener for å oppdatere visningen når vindusstørrelsen endres
+   window.addEventListener("resize", updateBreadcrumbsVisibility);
+}
+
+// Kaller funksjonen for å generere brødsmulemenyen
+generateBreadcrumbs();
+
+function generateMainMenu() {
+   const mainMenu = document.getElementById("mainMenu");
+
+   function createMenuItem(displayName, url) {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = url;
+      link.textContent = displayName;
+      listItem.appendChild(link);
+      return listItem;
+   }
+
+   function createSubMenu(children) {
+      const subMenu = document.createElement("ul");
+      for (const [url, value] of Object.entries(children)) {
+         const listItem = createMenuItem(value.displayName, url);
+         subMenu.appendChild(listItem);
+      }
+      return subMenu;
+   }
+
+   // Legger til "Hjem"-elementet i menyen
+   const homeMenuItem = createMenuItem(siteHierarchy["/index.html"].displayName, "/index.html");
+   mainMenu.appendChild(homeMenuItem);
+
+   // Legger til resten av elementene i menyen
+   for (const [url, value] of Object.entries(siteHierarchy["/index.html"].children)) {
+      const menuItem = createMenuItem(value.displayName, url);
+      mainMenu.appendChild(menuItem);
+
+      if (url === "/oppdragsbank.html" && value.children) {
+         const subMenu = createSubMenu(value.children);
+         menuItem.appendChild(subMenu);
+      }
+   }
+}
+
+generateMainMenu();
